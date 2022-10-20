ef327dfa8b6e0a0b3ce51235b298a391be568276
Anush <anushkumar.s@rently.com>
2022-10-20 13:53:46 +0530

    UI Integration

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,34 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+
+class ActionSetSatisfiedAsTrue(Action):
+
+    def name(self) -> Text:
+        return "action_set_satisfied_as_true"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        message = "Thanks for your support. Have a great day"
+        dispatcher.utter_message(text=message)
+        return []
+
+class ActionSetSatisfiedAsFalse(Action):
+
+    def name(self) -> Text:
+        return "action_set_satisfied_as_false"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        message = "Ok! Will conncet you with a live agent in a short"
+        dispatcher.utter_message(text=message)
+        return []
